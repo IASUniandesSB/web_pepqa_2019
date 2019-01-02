b4ed5140573bf8a9c8444a6889ac358765561c2c
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-01-02 09:20:58 -0500

    Update

--- dist/precache-manifest.647d65058fa6dfc280fcd52c6a287c04.js
+++ dist/precache-manifest.647d65058fa6dfc280fcd52c6a287c04.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": "/robots.txt"
+    "revision": "f46df4a5314c8f7f7d38e332207f4e71",
+    "url": "/img/slider/thumbs/hd_img_6.jpg"
   },
   {
     "revision": "ecb1186b4f85f981a8ac0fc201d955c7",
@@ -20,16 +20,16 @@ self.__precacheManifest = [
     "url": "/img/slider/thumbs/hd_img_0.jpg"
   },
   {
-    "revision": "51a6a3f85bd915c36a2cfde9ab16cdd2",
-    "url": "/img/slider/thumbs/hd_img_9.jpg"
+    "revision": "283e92d815856934be2a290cc73ef951",
+    "url": "/img/slider/thumbs/hd_img_2.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
   },
   {
     "revision": "3789d8c2e3d7f5349f42be7e8609d778",
     "url": "/img/slider/thumbs/hd_img_8.jpg"
-  },
-  {
-    "revision": "283e92d815856934be2a290cc73ef951",
-    "url": "/img/slider/thumbs/hd_img_2.jpg"
   },
   {
     "revision": "054cd053e6c8b6385e339e9ed4f2fd4f",
@@ -40,20 +40,16 @@ self.__precacheManifest = [
     "url": "/index.html"
   },
   {
+    "revision": "51a6a3f85bd915c36a2cfde9ab16cdd2",
+    "url": "/img/slider/thumbs/hd_img_9.jpg"
+  },
+  {
     "revision": "b34225622ff48ee0d9a36159144933f5",
     "url": "/img/slider/thumbs/hd_img_7.jpg"
   },
   {
-    "revision": "f46df4a5314c8f7f7d38e332207f4e71",
-    "url": "/img/slider/thumbs/hd_img_6.jpg"
-  },
-  {
     "revision": "61c0e357ff6c5d890348",
     "url": "/js/app.ca7e2b18.js"
-  },
-  {
-    "revision": "34d83fe5b92106c0236723132f5c39f7",
-    "url": "/img/slider/thumbs/hd_img_5.jpg"
   },
   {
     "revision": "d774a26d4f8fa7b8cdc1a413de8c50c7",
@@ -64,32 +60,36 @@ self.__precacheManifest = [
     "url": "/img/slider/thumbs/hd_img_3.jpg"
   },
   {
-    "revision": "31fc991ee7ec7867a5f737703b1ee719",
-    "url": "/img/slider/thumbs/hd_img_13.jpg"
-  },
-  {
     "revision": "3d8d068a5bc9c2f82d0ca7f3ba748531",
     "url": "/img/slider/thumbs/hd_img_14.jpg"
+  },
+  {
+    "revision": "34d83fe5b92106c0236723132f5c39f7",
+    "url": "/img/slider/thumbs/hd_img_5.jpg"
   },
   {
     "revision": "bf5ed73e418d98ef4446b2ef2670040f",
     "url": "/img/slider/thumbs/hd_img_12.jpg"
   },
   {
-    "revision": "c24537a3f6a99fccbec15049304004e7",
-    "url": "/img/slider/thumbs/hd_img_10.jpg"
-  },
-  {
-    "revision": "0bda4e700889494d4d8310cb905fde31",
-    "url": "/img/slider/thumbs/hd_img_1.jpg"
+    "revision": "31fc991ee7ec7867a5f737703b1ee719",
+    "url": "/img/slider/thumbs/hd_img_13.jpg"
   },
   {
     "revision": "0697413443aa972b4483f295af910913",
     "url": "/img/slider/thumbs/hd_img_11.jpg"
   },
   {
-    "revision": "e7bfd1d1db10b6317c9818f1f9b3d688",
-    "url": "/img/info-bg-pattern.e7bfd1d1.png"
+    "revision": "0bda4e700889494d4d8310cb905fde31",
+    "url": "/img/slider/thumbs/hd_img_1.jpg"
+  },
+  {
+    "revision": "c24537a3f6a99fccbec15049304004e7",
+    "url": "/img/slider/thumbs/hd_img_10.jpg"
+  },
+  {
+    "revision": "5da229148753cf50ff41dfdbeda5aead",
+    "url": "/img/logos/sponsors/sponsor_ias_uniandes_sb_logo.png"
   },
   {
     "revision": "32f6663fe53d5c18ded6de5ef3c45095",
@@ -100,16 +100,16 @@ self.__precacheManifest = [
     "url": "/img/slider/hd/hd_img_2.jpg"
   },
   {
-    "revision": "c3f2cfb1ac3695cf60f0c1f7bab16293",
-    "url": "/img/slider/hd/hd_img_12.jpg"
+    "revision": "42f8d12902f82d64ca8292be87ebb5fe",
+    "url": "/img/slider/hd/hd_img_13.jpg"
   },
   {
     "revision": "22a07ac623bf42c0edd98844bbd049dd",
     "url": "/img/slider/hd/hd_img_4.jpg"
   },
   {
-    "revision": "42f8d12902f82d64ca8292be87ebb5fe",
-    "url": "/img/slider/hd/hd_img_13.jpg"
+    "revision": "c3f2cfb1ac3695cf60f0c1f7bab16293",
+    "url": "/img/slider/hd/hd_img_12.jpg"
   },
   {
     "revision": "ae7715e60ef4cb5ab22b4cf169439ab7",
@@ -128,16 +128,24 @@ self.__precacheManifest = [
     "url": "/img/slider/hd/hd_img_10.jpg"
   },
   {
-    "revision": "50af941698b49ad7c9488d8071ce38c3",
-    "url": "/img/logos/sponsors/sponsor_ias_unal_manizales_sb_logo.png"
-  },
-  {
     "revision": "684e7fb9857c014dfa83206db42f89a5",
     "url": "/img/slider/hd/hd_img_1.jpg"
   },
   {
     "revision": "e6a0ea54d0bf442bb520a964fae42fd9",
     "url": "/img/slider/hd/hd_img_14.jpg"
+  },
+  {
+    "revision": "50af941698b49ad7c9488d8071ce38c3",
+    "url": "/img/logos/sponsors/sponsor_ias_unal_manizales_sb_logo.png"
+  },
+  {
+    "revision": "4544cc6059e29a5c9a0af3590002d6d4",
+    "url": "/img/slider/hd/hd_img_5.jpg"
+  },
+  {
+    "revision": "481e41a7d474b0ea8528adecb298c2dd",
+    "url": "/img/slider/hd/hd_img_11.jpg"
   },
   {
     "revision": "b493a4e925bd231344ed302de74e8b6a",
@@ -152,16 +160,8 @@ self.__precacheManifest = [
     "url": "/img/logos/technical-co-sponsors/sponsor_ias_logo.png"
   },
   {
-    "revision": "5da229148753cf50ff41dfdbeda5aead",
-    "url": "/img/logos/sponsors/sponsor_ias_uniandes_sb_logo.png"
-  },
-  {
-    "revision": "4544cc6059e29a5c9a0af3590002d6d4",
-    "url": "/img/slider/hd/hd_img_5.jpg"
-  },
-  {
-    "revision": "481e41a7d474b0ea8528adecb298c2dd",
-    "url": "/img/slider/hd/hd_img_11.jpg"
+    "revision": "f35b4194d1e7a040e960a3005257941b",
+    "url": "/img/slider/hd/hd_img_3.jpg"
   },
   {
     "revision": "d42d458ca2022dbf1715045c86139338",
@@ -172,40 +172,40 @@ self.__precacheManifest = [
     "url": "/img/logos/technical-co-sponsors/technical_co_sponsors_ieee_colombia_logo.png"
   },
   {
-    "revision": "f35b4194d1e7a040e960a3005257941b",
-    "url": "/img/slider/hd/hd_img_3.jpg"
-  },
-  {
-    "revision": "7e03e73642052a49640e2834fb1d1395",
-    "url": "/favicon/html_code.html"
-  },
-  {
-    "revision": "c19fc7e9fff8e2d810c26ff21ce5a04d",
-    "url": "/favicon/site.webmanifest"
-  },
-  {
-    "revision": "18059cde318e59e24e2176e7effe114d",
-    "url": "/img/header_logo.18059cde.png"
-  },
-  {
-    "revision": "e7abdfdca11241bb9028ed669fc6a720",
-    "url": "/img/logos/patrons/patron_unal_manizales_logo.jpg"
-  },
-  {
-    "revision": "ffb595095ef104fbe811c7e5e87502b9",
-    "url": "/img/bg-pattern.ffb59509.png"
-  },
-  {
-    "revision": "d2ea71f1b9dc39f66fe52693a28e18f1",
-    "url": "/favicon/safari-pinned-tab.svg"
+    "revision": "44aae40fc0b3a6542d45c476b914d836",
+    "url": "/favicon/favicon-32x32.png"
   },
   {
     "revision": "117d1d7518ac684810640fe300d7fb0b",
     "url": "/favicon/mstile-150x150.png"
   },
   {
-    "revision": "44aae40fc0b3a6542d45c476b914d836",
-    "url": "/favicon/favicon-32x32.png"
+    "revision": "ffb595095ef104fbe811c7e5e87502b9",
+    "url": "/img/bg-pattern.ffb59509.png"
+  },
+  {
+    "revision": "e7abdfdca11241bb9028ed669fc6a720",
+    "url": "/img/logos/patrons/patron_unal_manizales_logo.jpg"
+  },
+  {
+    "revision": "18059cde318e59e24e2176e7effe114d",
+    "url": "/img/header_logo.18059cde.png"
+  },
+  {
+    "revision": "c19fc7e9fff8e2d810c26ff21ce5a04d",
+    "url": "/favicon/site.webmanifest"
+  },
+  {
+    "revision": "d2ea71f1b9dc39f66fe52693a28e18f1",
+    "url": "/favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "e7bfd1d1db10b6317c9818f1f9b3d688",
+    "url": "/img/info-bg-pattern.e7bfd1d1.png"
+  },
+  {
+    "revision": "7e03e73642052a49640e2834fb1d1395",
+    "url": "/favicon/html_code.html"
   },
   {
     "revision": "2ae725d51ee68a8ee751805160a20a89",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.657066faa18eae0e5dbaffb4ec362cfd.js"
+  "/precache-manifest.647d65058fa6dfc280fcd52c6a287c04.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pepqa_web"});
